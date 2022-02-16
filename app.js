6bf8524d0baa6cca986863c998ee512dcbf4bfce
Pradumn Garg <pradumngarg4050@gmail.com>
2022-02-16 20:28:07 +0530

    Final commit

--- app.js
+++ app.js
@@ -3,14 +3,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose=require("mongoose");
 const _=require('lodash');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const homeStartingContent = "This is the first site I have created from scratch Independently and so I want it to be special. I'll  be talking  about the courses I have been doing and the projects I'll be making and how you guys could reciprocate the same. So if you are interested to take a piece of the pie I expect you to bookmark this site for future reference . I have made certain mistakes in the past and so don't want people to reiterate the same.  The analogy Learn from your mistakes  here fits perfectly. Pleased to welcome you all on board with us on this journey😊 "
 const app = express();
 let posts=[];
+mongoose.connect("mongodb+srv://pradumn-garg:Aarpga4050@cluster0.i1m3m.mongodb.net/blogDB");
+
+const postschema={
+  title :String,
+  content: String
+};
+
+const Post=mongoose.model(
+  "Post",postschema
+)
 
 app.set('view engine', 'ejs');
 
@@ -18,17 +26,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render('home',{'HomeContent': homeStartingContent,'postsList':posts});
-  
+  Post.find({}, function(err, posts){
+
+    res.render("home", {
+ 
+      HomeContent: homeStartingContent,
+ 
+      postsList: posts
+ 
+      });
+ 
+  });
+ 
+ 
+
 })
 
 
 app.get("/about",function(req,res){
-  res.render('about',{'AboutContent':aboutContent});
+  res.render('about');
 })
 
 app.get("/contact",function(req,res){
-  res.render('contact',{'ContactContent': contactContent});
+  res.render('contact');
 })
 
 app.get("/compose",function(req,res){
@@ -36,32 +56,48 @@ app.get("/compose",function(req,res){
 })
 
 
-app.get("/posts/:postsName",function(req,res){
-const requestedTitle=_.lowerCase(req.params.postsName);
+app.get("/posts/:postId",function(req,res){
 
-posts.forEach(function(post){
-  const storedTitle=_.lowerCase(post.title1);
-  if(storedTitle==requestedTitle){
-    res.render('post',{
-      title:post.title1,
-    content:post.post1
-    })
-  }else{
-    console.log("Not Found!!");
-  }
+const requestedPostId = req.params.postId;
 
-})
-})
+Post.findOne({_id: requestedPostId}, function(err, post){
+
+  res.render("post", {
+
+    title: post.title,
+
+    content: post.content
+
+  });
+
+});
+
+});
 
 app.post("/compose",function(req,res){
 
-  const compose={
-    title1:req.body.titlecontent,
-    post1:req.body.postcontent
-    };
+  const post = new Post ({
 
-    posts.push(compose);
-    res.redirect("/");
+    title: req.body.titlecontent,
+  
+    content: req.body.postcontent
+  
+  });
+  
+  post.save(function(err){
+
+    if (!err){
+ 
+      res.redirect("/");
+ 
+    }
+ 
+  });
+ 
+ 
+  
+
+
 })
 
 app.listen(process.env.PORT || 3000, function() {
